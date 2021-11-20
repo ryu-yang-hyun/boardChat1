3a161d027c2cab5e5add059d6a448f359a6e56ae
dbdidgus <sf_fs@naver.com>
2021-11-21 03:32:51 +0900

    vue 원복..

--- src/main/java/com/board/boardchat/controller/HomeController.java
+++ src/main/java/com/board/boardchat/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("")
+    @GetMapping("/vue")
     public String vue(){
 
         return "vue/index";
